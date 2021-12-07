4ec8aef9f8250bb102343ef86190b6fd21330589
Emin Kazım <eminjk@code.edu.az>
2021-12-07 05:06:18 -0800

    String match method with extention

---
+++ Static, extention, enum/Static, extention, enum/Helpers/Extention.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Static__extention__enum.Helpers
+{
+    public static class Extention
+    {
+
+
+        public static int MatchCount(this string str, string pattern)
+        {
+
+            int matches = Regex.Matches(str, pattern).Count;
+            return matches;
+
+        }
+    }
+}
+
---
+++ Static, extention, enum/Static, extention, enum/Program.cs
@@ -0,0 +1,15 @@
+﻿using Static__extention__enum.Helpers;
+using System;
+
+namespace Static__extention__enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "Cavid muellim 100 yazacaq mene";
+            Console.WriteLine(word.MatchCount("a"));
+
+        }
+    }
+}
